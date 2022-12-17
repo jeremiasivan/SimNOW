cc96a62cf52105ff1e0e0e01de77278b779abb6b
Ivan Jeremias <jeremias.ivan98@gmail.com>
2022-12-17 18:57:58 +1100

    Update SimSum

--- wrapper.R
+++ wrapper.R
@@ -2,10 +2,10 @@ library(doParallel)
 
 #################################
 # wrapper-specific parameters
-ms_r <- c(0,1,10)
-nreps <- 2
-prefix <- "sim"
-nthread <- 2
+ms_r <- c(30,300)
+nreps <- 5
+prefix <- "helibut"
+nthread <- 3
 
 # general
 rmddir <- "~/Documents/SimNOW/rmd"
@@ -15,7 +15,7 @@ redo <- TRUE
 # sequence simulation
 msdir <- "~/Documents/msdir/ms"
 ms_params <- "7 1 -T -I 7 1 1 1 1 1 1 1 -ej 10 2 1 -ej 24 3 1 -ej 35 4 1 -ej 32 5 6 -ej 53 6 1 -ej 69 7 1 -es 1 2 0.25 -ej 1 8 1 -es 23 3 0.25 -ej 23 9 4 -es 34 6 0.25 -ej 34 10 4"
-ms_l <- 1000000
+ms_l <- 10000000
   
 iqtree2dir <- "~/Downloads/iqtree-2.2.2.2-MacOSX/bin/iqtree2"
 alisim_model <- "JC"
@@ -23,7 +23,7 @@ alisim_scale <- 0.00029
 outgroup <- "7"
   
 # non-overlapping window analysis
-window_size <- c(50000,100000)
+window_size <- c(10000,20000,50000,100000,200000,500000,1000000)
 
 #################################
 
