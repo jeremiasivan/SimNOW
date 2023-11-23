4b79059282636bfcf5c7a25e8eeeca86d75de187
Jeremias Ivan <jeremias.ivan98@gmail.com>
2023-11-23 17:15:02 +1100

    Add run_all.R for UCSC Primates

--- empirical_analysis/datasets/ucsc_primates/codes/stepwise_now/run_all.R
+++ empirical_analysis/datasets/ucsc_primates/codes/stepwise_now/run_all.R
@@ -8,45 +8,40 @@ outdir <- ""
 redo <- FALSE
 
 # data_preparation.Rmd
-fn_hal <- ""
-fn_refseq <- paste0(codedir, "/datasets/edelman_etal_2019/files/refseq.txt")
-
-dir_hal2maf <- "~/hal2maf"
-dir_singleCopy <- "~/getSingleCopy.py"
-dir_mafsort <- "~/maf-sort.sh"
+fn_mafseq <- "~/SimNOW/empirical_analysis/datasets/ucsc_primates/files/mafseq.txt"
 dir_msaview <- "~/msa_view"
 
 # stepwise_now
-prefix <- ""
+prefix <- "primates"
 dir_iqtree2 <- "~/iqtree2"
 
-set_blmin <- FALSE
+set_blmin <- TRUE
 set_model <- FALSE
 dna_model <- ""
 
 bootstrap <- 1000
 bootstrap_type <- "ufboot"
-outgroup <- "HmelRef"
+outgroup <- ""
 
 initial_wsize <- 64000
 min_wsize <- 0
-min_informative_sites <- 4
+
+min_informative_sites <- 0
 #################################
 
 if (nthread / thread < 1) {
   stop("Invalid number of threads (nthread) vs thread per run (thread)")
 }
 
-# data conversion from HAL -> FASTA
-rmarkdown::render(input=paste0(codedir,"/datasets/edelman_etal_2019/codes/data_preparation.Rmd"),
+# data conversion from MAF -> FASTA
+rmarkdown::render(input=paste0(codedir,"/datasets/ucsc_primates/codes/data_preparation.Rmd"),
                   output_file=paste0(outdir,"/edelman.html"),
-                  params=list(fn_hal=fn_hal, fn_refseq=fn_refseq, thread=thread, outdir=outdir, redo=redo,
-                              dir_hal2maf=dir_hal2maf, dir_singleCopy=dir_singleCopy, dir_mafsort=dir_mafsort, dir_msaview=dir_msaview),
+                  params=list(fn_mafseq=fn_mafseq, thread=thread, outdir=outdir, redo=redo, dir_msaview=dir_msaview),
                   quiet=TRUE)
 
 # run NOW on every chromosome
-ls_chr <- list.dirs(outdir, full.names=F, recursive=F)
-ls_chr <- subset(ls_chr, grepl("chr+", ls_chr))
+ls_chr <- list.files(paste0(outdir,"/data/fasta/primates/"), pattern='*.fa$', full.names=F, recursive=F)
+ls_chr <- sapply(ls_chr, function(x) { gsub(".fa", "", x) })
 
 # create sets of parameters
 run_outdir <- paste0(outdir,"/",prefix,"/")
@@ -60,7 +55,7 @@ for (c in ls_chr) {
 
   # filtered sequence
   out <- paste0(currentdir,c,".html")
-  input_aln <- paste0(outdir,"/",c,"/fasta/concatenation/",c,"_concat.fa")
+  input_aln <- paste0(outdir,"/data/fasta/primates/",c,".fa")
   temprun <- list(out=out, params=list(codedir=codedir,
                                        prefix=c, outdir=run_outdir, thread=thread, redo=redo,
                                        iqtree2dir=dir_iqtree2, set_blmin=set_blmin, set_model=set_model, dna_model=dna_model, 
@@ -93,11 +88,5 @@ foreach(r=runs, .errorhandling = 'pass') %dopar% {
 }
 
 stopCluster(cl)
-
-# summary for all chromosomes
-rmarkdown::render(input=paste0(codedir,"/datasets/edelman_etal_2019/codes/stepwise_now/summary_all.Rmd"),
-                  output_file=paste0(outdir,"/",prefix,"/edelman_summary.html"),
-                  params=list(prefix=prefix, outdir=outdir, initial_wsize=initial_wsize),
-                  quiet=TRUE)
                   
 #################################
