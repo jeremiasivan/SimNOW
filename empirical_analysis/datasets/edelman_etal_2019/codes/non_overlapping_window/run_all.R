79065f641f4cef960bcc217a117d819374196c0f
Jeremias Ivan <jeremias.ivan98@gmail.com>
2023-11-28 20:10:52 +0700

    Remove empty topology

--- empirical_analysis/datasets/edelman_etal_2019/codes/non_overlapping_window/run_all.R
+++ empirical_analysis/datasets/edelman_etal_2019/codes/non_overlapping_window/run_all.R
@@ -92,7 +92,8 @@ for (c in ls_chr) {
   input_aln <- paste0(outdir,"/",c,"/fasta/concatenation/",c,"_concat_nogaps.fa")
   temprun <- list(out=out, params=list(codedir=codedir,
                                        prefix=no_gaps_c, outdir=run_outdir, thread=thread, redo=redo,
-                                       iqtree2dir=dir_iqtree2, set_blmin=set_blmin, set_model=set_model, dna_model=dna_model, outgroup=outgroup,
+                                       iqtree2dir=dir_iqtree2, set_blmin=set_blmin, set_model=set_model, dna_model=dna_model, 
+                                       bootstrap=bootstrap, bootstrap_type=bootstrap_type, outgroup=outgroup,
                                        input_aln=input_aln, window_len=window_len, window_size=window_size_nogaps, min_window_size=min_window_size_nogaps,
                                        ic_type=ic_type                                     
   ))
