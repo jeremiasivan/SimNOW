7177d95b7e9914be1ccc32e91bc8e08aacf73c19
Jeremias Ivan <jeremias.ivan98@gmail.com>
2025-03-23 19:33:24 +1100

    Clean-up Empirical Stepwise NOW

--- empirical_analysis/datasets/heliconius_erato/codes/run_all_stepwise_now.R
+++ empirical_analysis/datasets/heliconius_erato/codes/run_all_stepwise_now.R
@@ -10,42 +10,49 @@ outdir <- ""
 redo <- FALSE
 
 # data_preparation.Rmd
-fn_mafseq <- "~/SimNOW/empirical_analysis/datasets/hominidae/files/mafseq.txt"
+fn_hal <- ""
+fn_refseq <- paste0(codedir, "/datasets/heliconius_erato/files/refseq.txt")
+
+dir_hal2maf <- "~/hal2maf"
+dir_singleCopy <- "~/getSingleCopy.py"
+dir_mafsort <- "~/maf-sort.sh"
 dir_msaview <- "~/msa_view"
 
 # stepwise_now
-prefix <- "primates"
+prefix <- ""
 dir_iqtree2 <- "~/iqtree2"
 
-set_blmin <- TRUE
+set_blmin <- FALSE
 set_model <- FALSE
 dna_model <- ""
 
 bootstrap <- 1000
 bootstrap_type <- "ufboot"
-outgroup <- ""
+outgroup <- "HmelRef"
 
 initial_wsize <- 64000
 min_wsize <- 0
+min_informative_sites <- 4
 
-min_informative_sites <- 0
+# summary
+colour_scheme <- paste0(codedir, "/datasets/heliconius_erato/files/colour_scheme.txt")
 
-colour_scheme <- paste0(codedir, "/datasets/hominidae/files/colour_scheme.txt")
 #################################
 
 if (nthread / thread < 1) {
   stop("Invalid number of threads (nthread) vs thread per run (thread)")
 }
 
-# data conversion from MAF -> FASTA
-rmarkdown::render(input=paste0(codedir,"/datasets/hominidae/codes/data_preparation.Rmd"),
-                  output_file=paste0(outdir,"/hominidae.html"),
-                  params=list(fn_mafseq=fn_mafseq, thread=nthread, outdir=outdir, redo=redo, dir_msaview=dir_msaview),
+# data conversion from HAL -> FASTA
+rmarkdown::render(input=paste0(codedir,"/datasets/heliconius_erato/codes/data_preparation.Rmd"),
+                  output_file=paste0(outdir,"/heliconius.html"),
+                  params=list(fn_hal=fn_hal, fn_refseq=fn_refseq, thread=thread, outdir=outdir, redo=redo,
+                              dir_hal2maf=dir_hal2maf, dir_singleCopy=dir_singleCopy, dir_mafsort=dir_mafsort, dir_msaview=dir_msaview),
                   quiet=TRUE)
 
 # run NOW on every chromosome
-ls_chr <- list.files(paste0(outdir,"/data/fasta/primates/"), pattern='*.fa$', full.names=F, recursive=F)
-ls_chr <- sapply(ls_chr, function(x) { gsub(".fa", "", x) })
+ls_chr <- list.dirs(outdir, full.names=F, recursive=F)
+ls_chr <- subset(ls_chr, grepl("chr+", ls_chr))
 
 # create sets of parameters
 run_outdir <- paste0(outdir,"/",prefix,"/")
@@ -59,7 +66,7 @@ for (c in ls_chr) {
 
   # filtered sequence
   out <- paste0(currentdir,c,".html")
-  input_aln <- paste0(outdir,"/data/fasta/primates/",c,".fa")
+  input_aln <- paste0(outdir,"/",c,"/fasta/concatenation/",c,"_concat.fa")
   temprun <- list(out=out, params=list(codedir=codedir,
                                        prefix=c, outdir=run_outdir, thread=thread, redo=redo,
                                        iqtree2dir=dir_iqtree2, set_blmin=set_blmin, set_model=set_model, dna_model=dna_model, 
@@ -94,10 +101,10 @@ foreach(r=runs, .errorhandling = 'pass') %dopar% {
 stopCluster(cl)
 
 # summary for all chromosomes
-rmarkdown::render(input=paste0(codedir,"/datasets/hominidae/codes/stepwise_now/summary_all.Rmd"),
-                  output_file=paste0(outdir,"/",prefix,"/hominidae_summary.html"),
-                  params=list(codedir=codedir, prefix=prefix, outdir=outdir, thread=nthread, redo=redo,
+rmarkdown::render(input=paste0(codedir,"/stepwise_now/4_summary_all.Rmd"),
+                  output_file=paste0(outdir,"/",prefix,"/heliconius_summary.html"),
+                  params=list(codedir=codedir, prefix=prefix, thread=thread, outdir=outdir,
                               initial_wsize=initial_wsize, min_wsize=min_wsize, colour_scheme=colour_scheme),
                   quiet=TRUE)
-
+                  
 #################################
